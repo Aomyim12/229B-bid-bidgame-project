7986d7d325a3955571bc44bb52cfb3a9ddf25e7f
Aomyim12 <hsamama98@gmail.com>
2024-03-23 21:23:55 +0700

    Map3

---
+++ Assets/Scripts/public float blowSpeed = 1.0f;      void Update()     {          เปลี่ยนตำแหน่งของ GameObject ในแนวแกน y เพื่อเป่า         transform.Translate(0, blowSpeed  Time.deltaTime, 0);     }.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class publicfloatblowSpeed=1.0f;voidUpdate(){เปลี่ยนตำแหน่งของGameObjectในแนวแกนyเพื่อเป่าtransform.Translate(0,blowSpeedTime.deltaTime,0);} : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
