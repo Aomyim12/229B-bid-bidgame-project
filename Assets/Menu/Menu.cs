b0addc24e4c5190ef7af50653953eaa7fc642ea8
Aomyim12 <hsamama98@gmail.com>
2024-03-24 06:30:11 +0700

    Menu

---
+++ Assets/Menu/Menu.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject creditPanel;
+
+    void Start()
+    {
+        // รับ Component ของปุ่ม Play
+        Button playButton = GameObject.Find("PlayButton").GetComponent<Button>();
+
+        // กำหนดเหตุการณ์เมื่อคลิกปุ่ม Play
+        playButton.onClick.AddListener(PlayGame);
+
+        // รับ Component ของปุ่ม Quit
+        Button quitButton = GameObject.Find("QuitButton").GetComponent<Button>();
+
+        // กำหนดเหตุการณ์เมื่อคลิกปุ่ม Quit
+        quitButton.onClick.AddListener(QuitGame);
+
+        // รับ Component ของปุ่ม Credit
+        Button creditButton = GameObject.Find("CreditButton").GetComponent<Button>();
+
+        // กำหนดเหตุการณ์เมื่อคลิกปุ่ม Credit
+        creditButton.onClick.AddListener(ToggleCreditPanel);
+    }
+
+    // ฟังก์ชันเริ่มเล่นเกม
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    // ฟังก์ชันออกจากเกม
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    // ฟังก์ชันแสดง/ซ่อน Panel ของเครดิต
+    public void ToggleCreditPanel()
+    {
+        creditPanel.SetActive(!creditPanel.activeSelf);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ตรวจสอบการกดปุ่ม Esc เพื่อปิด Panel ของเครดิต
+        if (Input.GetKeyDown(KeyCode.Escape) && creditPanel.activeSelf)
+        {
+            ToggleCreditPanel();
+        }
+    }
+}
--- Assets/Scripts/BottonPress.cs
+++ Assets/Scripts/BottonPress.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class BottonPress : MonoBehaviour
 {
     public ObjectExplosion[] objectsToExplode; // วัตถุที่ต้องการให้ระเบิดเมื่อกดปุ่ม
+    [SerializeField] private bool hasWon = false;
+    [SerializeField] private GameObject YouWin;
 
     private void OnMouseUpAsButton()
     {
-      
-            // วนลูปผ่านวัตถุที่ต้องการให้ระเบิดทั้งหมด
-            foreach (ObjectExplosion obj in objectsToExplode)
+        if (hasWon) // ตรวจสอบว่ายังไม่เคยชนะ
+        {
+            WinGame(); // เรียกใช้เมทอดเพื่อประกาศชนะ
+        }
+        // วนลูปผ่านวัตถุที่ต้องการให้ระเบิดทั้งหมด
+        foreach (ObjectExplosion obj in objectsToExplode)
             {
                 // เรียกใช้เมธอด Explode() ในทุก ๆ วัตถุที่ต้องการระเบิด
                 obj.Explode();
             }
         
+
         Debug.Log("ddd");
     }
-        
-    
+    private void WinGame()
+    {
+        Debug.Log("ชนะ"); // แสดงข้อความ "ชนะ" ใน Console
+        YouWin.SetActive(true);
+        hasWon = false; // ตั้งค่าเป็น true เมื่อชนะ เพื่อป้องกันการชนะซ้ำ
+
+    }
+
 }
